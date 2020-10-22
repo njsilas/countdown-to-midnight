b969b48d946d4a1f5829ffe21778a9a8b34302c2
Nicholas Silas <njsilas@gmail.com>
2020-10-22 15:42:02 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
-#write your code here
 
-def countdown
+def countdown(time)
+    while time >= 1
+        puts "#{time} SECOND(S)!"
+        time -= 1
+    
+    end
+    
+"HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(time)
+    while time >=1
+        sleep(1)
+        time -= 1
+    end
+"HAPPY NEW YEAR!"
+end
+
+
